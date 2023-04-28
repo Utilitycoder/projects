d205dd16d65b8f80dca56540835f4a221ca829a3
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-04-28 21:34:03 +0100

    chore: linked everything together

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -3,7 +3,16 @@ use std::{fs, error::Error};
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{}", contents);
+    // if search returns an empty vector, print a message
+    let results = search(&config.query, &contents);
+
+    if results.len() == 0 {
+        println!("No results found for '{}'", config.query);
+    }
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
@@ -24,5 +33,56 @@ impl Config {
         let file_path = args[2].clone();
 
         Ok(Config { query, file_path })
+    }
+}
+
+pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_build_config() {
+        let args = vec![
+            String::from("minigrep"),
+            String::from("query"),
+            String::from("file_path"),
+        ];
+
+        let config = Config::build(&args).unwrap();
+
+        assert_eq!(config.query, "query");
+        assert_eq!(config.file_path, "file_path");
+    }
+
+    #[test]
+    fn test_build_config_not_enough_args() {
+        let args = vec![
+            String::from("minigrep"),
+            String::from("query"),
+        ];
+
+        let config = Config::build(&args);
+
+        assert_eq!(config.is_err(), true);
+    }
+
+    #[test]
+    fn test_search() {
+        let query = "duct";
+        let contents = "\n Rust:\nsafe, fast, productive.\nPick three.\nDuct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
