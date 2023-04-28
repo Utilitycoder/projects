a40cd6e8772e561eee9da54f38e883c862c49ad9
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-04-28 20:54:34 +0100

    chore: seperated logic from main functions

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,34 +1,16 @@
-use std::env;
-use std::fs;
+use minigrep::{Config, run};
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|e| {
+        println!("Problem parsing arguments: {}", e);
+        process::exit(1);
+    });
 
-    println!("Searching for {} in file {}\n", config.query, config.file_path);
-
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-
-        if args.len() < 3 {
-            panic!("Not enough arguments");
-        }
-        
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config { query, file_path }
-    }
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    };
 }
